f996b063a7407f4d81d9c74763423e8bd3450870
danbe8080 <danbe8080@gmail.com>
2024-06-06 17:56:04 +0900

    페이지4 반응형 완료

--- src/components/MainFourComponent.vue
+++ src/components/MainFourComponent.vue
@@ -1,247 +1,210 @@
 <template>
-  <q-card
-    flat
-    class="row items-start text-center"
-    style="height: 100svh; padding: 0; color: #72605c"
-  >
-    <div class="col-12 bg_img_wrapper">
+  <q-card flat class="four_wrapper">
+    <div class="bg_img_wrapper">
       <q-img
         src="../assets/page4/page4_bg.jpg"
-        loading="lazy" spinner-color="white"
-        style="width: 100%; height: 100%"
+        loading="lazy"
+        spinner-color="white"
+        fit="fill"
+        class="ba_img"
       />
     </div>
 
-    <div class="col-12 q-pa-sm justify-center img_list_wrapper">
-      <q-card
-        flat
-        style="
-          width: 100%;
-          height: 100%;
-          border: 2px solid #dbc7c2;
-          box-sizing: border-box;
-          padding: 5px 5px;
-        "
-        class="row justify-around items-center"
-      >
+    <div class="img_list_wrapper">
+      <q-card flat class="list_wrap row justify-evenly items-center">
         <div
-          v-for="(num, idx) in IMG_ROW_NUM"
+          v-for="(num,idx) in allUrlObjList"
           :key="`text${num}`"
-          class="col-12 row justify-evenly"
-          style="height: 32%"
-        >
-          <div
-            v-for="(sm, i) in IMG_ROW_NUM"
-            :key="`text${sm}`"
-            class="col-3"
-            style="width: 31%; height: 100%"
-            @click="onClickImg(idx, i)"
-          >
-            <div
-              class="bg_text"
-              :style="{
-                backgroundImage: `url(${rowUrlObjList[idx][i].weddingImg})`,
+          class="col-12 row justify-evenly photo_img"
+          :style="{
+                backgroundImage: `url(${num.weddingIcon})`,
               }"
-            ></div>
-          </div>
+          @click="onClickImg(idx)"
+        >
         </div>
       </q-card>
-
-
     </div>
 
-    <div class="col-12 wedding_info_wrapper column">
-      <div class="wedding_info_text w-100">
-        자세한 예식 정보는 <span style="color: #dbc7c2">“예식정보 보기”</span> 클릭!
+        <div class="wedding_info_wrapper">
+      <div class="wedding_info_text">
+        자세한 예식 정보는
+        <span style="color: #dbc7c2">“예식정보 보기”</span> 클릭!
       </div>
-      <div class="w-100" style="width: 32px; height: 22px">
-          <q-img
-            src="../assets/page4/page4_arrow2.png"
-            loading="lazy" spinner-color="white"
-            style="width: 100%; height: 100%"/>
+      <div style="width: 3dvh;">
+        <q-img
+          src="../assets/page4/page4_arrow2.png"
+          loading="lazy"
+          spinner-color="white"
+          fit="fill"
+        />
       </div>
     </div>
 
-
-    <q-dialog v-model="icon">
-      <q-card style="width: 100vw; height: 60vh; display: flex; align-items: center; justify-content: center; border-radius: 15px;">
-          <q-carousel
-            ref="carousel"
-            v-model="slide"
-            swipeable
-            arrows
-            infinite
-            padding
-            style="width: 100vw; height: 100%; padding: 15px;"
-          >
-            <q-carousel-slide v-for="(img, idx) in allUrlObjList" :key="img.weddingPhoto" :name="idx + 1" style="padding: 0; border-radius: 15px;" :img-src="`${img.weddingPhoto}`" />
-
-          </q-carousel>
-
+    <q-dialog v-model="icon" full-width>
+      <q-card
+        class="dialog_wrap"
+      >
+        <q-carousel
+          ref="carousel"
+          v-model="slide"
+          animated
+          swipeable
+          arrows
+          infinite
+          padding
+          style="width: 100vw; height: 100%; padding: 15px"
+        >
+          <q-carousel-slide
+            v-for="(img, idx) in allUrlObjList"
+            :key="img.weddingPhoto"
+            :name="idx + 1"
+            style="padding: 0; border-radius: 15px"
+            :img-src="`${img.weddingPhoto}`"
+          />
+        </q-carousel>
       </q-card>
     </q-dialog>
-
   </q-card>
-
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const  slide = ref(1);
-const IMG_ROW_NUM = 3;
+const slide = ref(1);
 
-
-const rowUrlObjList = [
-  [
-    // 첫번째 줄
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_1.png',
-        import.meta.url
-      ).href,
-    },
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_2.png',
-        import.meta.url
-      ).href,
-    },
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_3.png',
-        import.meta.url
-      ).href,
-    },
-  ],
-  [
-    // 두번째 줄
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_4.png',
-        import.meta.url
-      ).href,
-    },
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_5.png',
-        import.meta.url
-      ).href,
-    },
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_6.png',
-        import.meta.url
-      ).href,
-    },
-  ],
-  [
-    // 세번째 줄
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_7.png',
-        import.meta.url
-      ).href,
-    },
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_8.png',
-        import.meta.url
-      ).href,
-    },
-    {
-      weddingImg: new URL(
-        '../assets/page4/Page4_Photo_icon_9.png',
-        import.meta.url
-      ).href,
-    },
-  ],
-];
 const allUrlObjList = [
   {
-    weddingPhoto:  new URL(
-      '../assets/page4/Photo_1.jpg',
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_1.png', import.meta.url).href,
+    weddingPhoto: new URL('../assets/page4/Photo_1.jpg', import.meta.url).href,
+  },
+  {
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_2.png', import.meta.url).href,
+    weddingPhoto: new URL('../assets/page4/Photo_2.jpg', import.meta.url).href,
+  },
+  {
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_3.png', import.meta.url).href,
+    weddingPhoto: new URL('../assets/page4/Photo_3.jpg', import.meta.url).href,
+  },
+  {
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_4.png', import.meta.url).href,
+
+    weddingPhoto: new URL('../assets/page4/Photo_4.jpg', import.meta.url).href,
+  },
+  {
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_5.png', import.meta.url).href,
+
+    weddingPhoto: new URL(
+      '../assets/page4/Photo_5.jpg',
       import.meta.url
     ).href,
   },
   {
-    weddingPhoto:  new URL(
-      '../assets/page4/Photo_2.jpg',
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_6.png', import.meta.url).href,
+
+    weddingPhoto: new URL(
+      '../assets/page4/Photo_6.jpg',
       import.meta.url
     ).href,
   },
   {
-    weddingPhoto:  new URL(
-      '../assets/page4/Photo_3.jpg',
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_7.png', import.meta.url).href,
+
+    weddingPhoto: new URL(
+      '../assets/page4/Photo_7.jpg',
       import.meta.url
     ).href,
   },
   {
-    weddingPhoto:  new URL(
-      '../assets/page4/Photo_4.jpg',
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_8.png', import.meta.url).href,
+
+    weddingPhoto: new URL(
+      '../assets/page4/Photo_8.jpg',
       import.meta.url
     ).href,
   },
-]
+  {
+    weddingIcon: new URL('../assets/page4/Page4_Photo_icon_9.png', import.meta.url).href,
+
+    weddingPhoto: new URL(
+      '../assets/page4/Photo_9.jpg',
+      import.meta.url
+    ).href,
+  },
+];
 const icon = ref(false);
-function onClickImg(rowIdx: number, colIdx: number) {
-  console.log('이미지 클릭');
+function onClickImg(idx: number) {
   icon.value = true;
-  let slideNum = ref(0);
-  switch (rowIdx) {
-    case 0:
-      slideNum.value = colIdx + 1;
-      break;
-
-    case 1:
-      slideNum.value = IMG_ROW_NUM + (colIdx + 1);
-      break;
-
-    case 2:
-      slideNum.value = (IMG_ROW_NUM * 2) + (colIdx + 1);
-      break;
-  }
-
-  slide.value = slideNum.value;
+  slide.value = idx + 1;
 }
-
 </script>
 <style scoped>
+.four_wrapper {
+  height: 100dvh;
+  padding: 0;
+  color: #72605c;
+}
+
 /*뒷배경 이미지*/
 .bg_img_wrapper {
   width: 100%;
-  height: 40svh;
+  height: 35dvh;
 
-  @media screen and (max-width: 578px) {
-    height: 38svh;
-  }
-  @media (min-width: 578px) and (max-width: 1023px) {
-    height: 35svh;
+  /* 태블릿 크기 */
+  @media (min-width: 991px) {
+    height: 35dvh;
   }
 
-  @media (min-width: 1024px) and (max-width: 1044px) {
-    height: 35svh;
+  /* 아이폰 크기 */
+  @media (min-width: 765px) and (max-width: 991px) {
+    height: 40dvh;
   }
+
+
+  /* 아이폰 크기 */
+  @media (min-width: 390px) and (max-width: 991px) {
+    height: 40dvh;
+  }
+}
+.ba_img {
+  width: 100%;
+  height: 100%;
 }
 
 .img_list_wrapper {
   width: 100%;
+  height: 58dvh;
   position: relative;
-  height: 50svh;
-  top: -8%;
+  top: -10dvh;
+  padding: 1.5dvh;
+  /* 컴퓨터 크기 */
+  @media (min-width: 991px) {
+    height: 66dvh;
+    padding: 2dvh;
+  }
+  /* 패드 크기 */
+  @media (min-width: 710px) and (max-width: 990px) {
+    height: 58dvh;
+  }
 
-  @media screen and (max-width: 578px) {
-    height: 50svh;
-    top: -8%;
+  /* 아이폰 크기 */
+  @media (min-width: 390px) and (max-width: 710px) {
+    height: 55dvh;
   }
-  @media (min-width: 578px) and (max-width: 1023px) {
-    height: 58svh;
-    top: -10%;
-  }
+}
 
-  @media (min-width: 1024px) and (max-width: 1044px) {
-    height: 62svh;
-    top: -10%;
-  }
+.list_wrap {
+  width: 100%;
+  height: 100%;
+  border: 2px solid #dbc7c2;
+  box-sizing: border-box;
+  //padding: 1dvh;
+}
+
+.photo_img {
+  width: 31%;
+  height: 31%;
+  background-size: 100% 100%;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
 .bg_text {
@@ -253,20 +216,43 @@ function onClickImg(rowIdx: number, colIdx: number) {
 /* 예식 정보 텍스트 */
 .wedding_info_wrapper {
   width: 100%;
-  height: 10%;
+  height: 5dvh;
   position: relative;
-  top: -10%;
+  top: -10dvh;
   display: flex;
+  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  font-size: 5vw;
-  @media (min-width: 578px) and (max-width: 1023px) {
-    font-size: 4vw;
+  font-size: 2.5dvh;
+  letter-spacing: -1px;
+  /* 컴퓨터 크기 */
+  @media (min-width: 991px) {
+    height: 4dvh;
+    font-size: 1.5dvh;
+    top: -11dvh;
+  }
+  /* 패드 크기 */
+  @media (min-width: 710px) and (max-width: 990px) {
+    height: 4dvh;
+    font-size: 1.5dvh;
+  }
+
+  @media (min-width: 578px) and (max-width: 709px) {
+    font-size: 18px;
     top: -11%;
   }
 }
 .wedding_info_text {
   letter-spacing: -1px;
   margin-bottom: 0;
+}
+
+/* 사진 팝업 css */
+.dialog_wrap {
+  height: 80dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
 }
 </style>
